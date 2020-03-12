eadb1a78845d435acbf66d2105962e5ce01b6f4a
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-03-12 20:03:56 +0200

    added apollo-client

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient, { gql } from 'apollo-boost'
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: 'http://localhost:4000'
+})
+const coursesRawQuery = gql`
+  {
+    courseFeed {
+      id
+      description
+      name
+    }
+  }
+`;
+console.log(coursesRawQuery);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+client.query({
+  query: gql`
+  {
+    courseFeed {
+      id
+      description
+      name
+    }
+  }
+  `
+})
+  .then(results => console.log(results))
+ReactDOM.render(<App />, document.getElementById('root'))
