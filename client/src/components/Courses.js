f7ffee6c1349abe0f2888da1832062bd3ad19601
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-03-16 11:06:16 +0200

    require auth on edit and delete

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -2,73 +2,73 @@ import React from 'react'
 import { Query, Mutation } from 'react-apollo'
 import { gql } from 'apollo-boost'
 import { Link } from 'react-router-dom';
+import { AUTH_TOKEN } from '../constants'
 
-// const renderCourses = ({ data, error, loading }) => {
-//   if (loading) return <p>...Loading</p>
-//   if (error) return <p>Error</p>
-//   return data.courseFeed.map(({ id, description, name, isPublished }) => {
-//     return (
-//       <div key={id}>
-//         <p>Name: {name}, Description: {description}</p>
-//       </div>
-//     )
-//   })
-// }
-const Courses = () => {
-  return (
-    <div>
-      <Query
-        query={COURSE_FEED_QUERY}
-      >
-        {/*{renderCourses} */}
-        {({ data, error, loading }) => {
-          if (loading) return <p>...Loading</p>;
-          if (error) return <p>Error</p>;
-          return data.courseFeed.map(
-            ({ id, description, name, isPublished }) => {
-              return (
-                <div key={id} className="card container">
-                  <div className="card-body">
-                    <div className="card-title"><p>{name}</p></div>
-                    <div className="card-text">{description}</div>
-                    <Link to={`course/${id}/edit`} className="btn btn-outline-secondary btn-sm">
-                      Edit
-                  </Link>
-                    <Mutation mutation={DELETE_COURSE_MUTATION}
-                      variables={{ id }}
-                      update={(cache, { data: { deleteCourse } }) => {
-                        const { courseFeed } = cache.readQuery({
-                          query: COURSE_FEED_QUERY
-                        })
-                        cache.writeQuery({
-                          query: COURSE_FEED_QUERY,
-                          data: {
-                            courseFeed: courseFeed.filter(course => course.id !== deleteCourse.id)
-                          }
-                        })
-                      }}
-                    >
-                      {(deleteCourse, { data, error, loading }) => {
-                        return (
-                          <button style={{ marginLeft: '10px' }}
-                            className="btn btn-outline-danger btn-sm"
-                            onClick={async () => {
-                              await deleteCourse()
-                            }}
-                          >Delete
-                          </button>
-                        )
-                      }}
-                    </Mutation>
+class Courses extends React.Component {
+  render() {
+    const authToken = localStorage.getItem(AUTH_TOKEN);
+    return (
+      <div>
+        <Query
+          query={COURSE_FEED_QUERY}
+        >
+          {/*{renderCourses} */}
+          {({ data, error, loading }) => {
+            if (loading) return <p>...Loading</p>;
+            if (error) return <p>Error</p>;
+            return data.courseFeed.map(
+              ({ id, description, name, isPublished }) => {
+                return (
+                  <div key={id} className="card container">
+                    <div className="card-body">
+                      <div className="card-title"><p>{name}</p></div>
+                      <div className="card-text">{description}</div>
+                      {
+                        authToken ? (
+                          <React.Fragment>
+                            <Link to={`course/${id}/edit`}
+                              className="btn btn-outline-secondary btn-sm">
+                              Edit
+                            </Link>
+                            <Mutation mutation={DELETE_COURSE_MUTATION}
+                              variables={{ id }}
+                              update={(cache, { data: { deleteCourse } }) => {
+                                const { courseFeed } = cache.readQuery({
+                                  query: COURSE_FEED_QUERY
+                                })
+                                cache.writeQuery({
+                                  query: COURSE_FEED_QUERY,
+                                  data: {
+                                    courseFeed: courseFeed.filter(course => course.id !== deleteCourse.id)
+                                  }
+                                })
+                              }}
+                            >
+                              {(deleteCourse, { data, error, loading }) => {
+                                return (
+                                  <button style={{ marginLeft: '10px' }}
+                                    className="btn btn-outline-danger btn-sm"
+                                    onClick={async () => {
+                                      await deleteCourse()
+                                    }}
+                                  >Delete
+                                  </button>
+                                )
+                              }}
+                            </Mutation>
+                          </React.Fragment>
+                        ) : null
+                      }
+                    </div>
                   </div>
-                </div>
-              );
-            }
-          );
-        }}
-      </Query>
-    </div>
-  )
+                );
+              }
+            );
+          }}
+        </Query>
+      </div>
+    )
+  }
 }
 export const DELETE_COURSE_MUTATION = gql`
 mutation DeleteCourse($id: ID!) {
